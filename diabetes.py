8007ceae439c1f9cb2e79cd76e967f679212f644
mariam <mis9424@nyu.edu>
2026-02-11 16:29:50 +0000

    v2

--- diabetes.py
+++ diabetes.py
@@ -8,7 +8,7 @@ import numpy as np
 import seaborn as sns
 
 from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+#from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(
     page_title="Diabetes Dashboard 🩺",
